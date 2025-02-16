edde051bf3e94a4f9c9d9001606fb27a7cbd1f42
AlessandroDetta3BHIF <alessandro.detta04@gmail.com>
2025-02-16 21:54:11 +0100

    Added the navbar and laid basic structure

---
+++ portfolio-dance/app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Tajawal } from "next/font/google";
+import { Sora } from "next/font/google";
+export const tajawal = Tajawal({
+  weight: '200'
+});
+
+export const sora = Sora({
+    weight: '400',
+    subsets: ["latin"]
+});
+  
---
+++ portfolio-dance/app/ui/navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { tajawal } from "@/app/ui/fonts";
+ export const Navbar = () =>{
+    return (
+        <nav className="bg-black-800 text-white p-4 sm:p-4 md:flex md:justify-between md:items-center">
+           <div className="container mx-auto flex justify-between items-center">
+            <a href="" className={`${tajawal.className} antialiased bg-black text-5xl font-bold`}>
+                Anna Deganello
+            </a>
+            <div className="hidden md:flex">
+                <Link href='/' className="mx-2 hover:bg-white hover:text-black px-10 py-1 rounded">
+                    About
+                </Link>
+                <Link href='/gallery' className="mx-2 hover:bg-white hover:text-black px-10 py-1 rounded">
+                    Gallery
+                </Link>
+                <Link href='/contact' className="mx-2 hover:bg-white hover:text-black px-10 py-1 rounded">
+                    Contact
+                </Link>
+            </div>
+           </div>
+        </nav>
+    );
+ };
